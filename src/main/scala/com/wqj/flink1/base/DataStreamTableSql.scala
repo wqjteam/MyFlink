bcaa9f2313da8acdf4af3dd641df6eb628b13185
githubwqj2 <2812930196@qq.com>
2020-10-09 18:27:59 +0800

    kafka提交

--- src/main/scala/com/wqj/flink1/base/DataStreamTableSql.scala
+++ src/main/scala/com/wqj/flink1/base/DataStreamTableSql.scala
@@ -1,5 +1,44 @@
 package com.wqj.flink1.base
 
-object DataStreamTableSql {
 
+import java.util.Properties
+
+import com.wqj.flink1.base.WcKafka.{broker, group_id, topic, zk}
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.{CheckpointingMode, TimeCharacteristic}
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.table.api.Table
+import org.apache.flink.table.api.scala.{BatchTableEnvironment, StreamTableEnvironment}
+import org.apache.log4j.Logger
+
+object DataStreamTableSql {
+  private val zk = "flinkmaster:2181"
+  private val broker = "flinkmaster:9092"
+  private val group_id = "wc1"
+  private val topic = "flink_test"
+
+  def main(args: Array[String]): Unit = {
+    lazy val logger = Logger.getLogger(WcKafka.getClass)
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val tableEnv = StreamTableEnvironment.create(env)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    env.enableCheckpointing(5000)
+    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
+    val properties = new Properties()
+    properties.setProperty("zookeeper.connect", zk)
+    properties.setProperty("bootstrap.servers", broker)
+    properties.setProperty("group.id", group_id)
+    //kafka的consumer，test1是要消费的topic
+    val kafkaSource = new FlinkKafkaConsumer(topic, new SimpleStringSchema, properties)
+    val stream = env.addSource(kafkaSource).map(x => {
+      val field = x.split(",")
+      person(1, "1", 3)
+    })
+
+   val table=tableEnv.fromDataStream(stream)
+    tableEnv.createTemporaryView("person",table)
+    tableEnv.sqlQuery("");
+  }
 }
