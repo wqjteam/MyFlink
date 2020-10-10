d62cca9176859d72b88f6a6fe51c964180faf530
githubwqj2 <2812930196@qq.com>
2020-10-10 17:50:10 +0800

    kafka提交

--- src/main/scala/com/wqj/flink1/base/OpeaterHive.scala
+++ src/main/scala/com/wqj/flink1/base/OpeaterHive.scala
@@ -1,5 +1,17 @@
 package com.wqj.flink1.base
 
-class OpeaterHive {
+import org.apache.flink.table.api.EnvironmentSettings
+import org.apache.flink.table.api.TableEnvironment
+import org.apache.flink.table.catalog.hive.HiveCatalog
 
+object OpeaterHive {
+  def main(args: Array[String]): Unit = {
+    val settings = EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build()
+    val tableEnv = TableEnvironment.create(settings)
+    val hive = new HiveCatalog("myhive", "ods", "", "2.3.7")
+    tableEnv.registerCatalog("myhive",hive)
+    tableEnv.useCatalog("myhive")
+    val result=tableEnv.sqlQuery("select * from ods.student")
+    tableEnv.execute("OpeaterHive")
+  }
 }
