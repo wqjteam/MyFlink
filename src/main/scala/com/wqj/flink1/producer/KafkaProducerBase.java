b97585abd1019194080b359955032bdbaa368d30
githubwqj2 <2812930196@qq.com>
2020-10-13 14:47:08 +0800

    kafka提交

--- src/main/scala/com/wqj/flink1/producer/KafkaProducerBase.java
+++ src/main/scala/com/wqj/flink1/producer/KafkaProducerBase.java
@@ -2,6 +2,7 @@ package com.wqj.flink1.producer;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+
 import java.util.Properties;
 
 public class KafkaProducerBase {
@@ -11,18 +12,21 @@ public class KafkaProducerBase {
         kafkaProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         kafkaProperties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         kafkaProperties.put("request.required.acks", "-1");
-         KafkaProducer producer = new KafkaProducer(kafkaProperties);
+        KafkaProducer producer = new KafkaProducer(kafkaProperties);
         producer = new KafkaProducer<String, String>(kafkaProperties);
 
         String data = "1,zs,1";
+//        data = "li,zs,ww";
+        String topic = "flink_test";
+//        topic = "flink_test_student";
         try {
             int i = 0;
 
-            while (i < 500) {
+            while (i < 2000) {
                 producer.send(new ProducerRecord<
-                        String, String>("flink_test",data));
-                System.out.println("输出第"+i+"次");
-                Thread.sleep(500);
+                        String, String>(topic, data));
+                System.out.println("输出第" + i + "次");
+                Thread.sleep(50);
                 i++;
             }
         } catch (Exception e) {
